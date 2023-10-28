b0db5912851ad79fd5b4b4498ec6d7e2a9b0a628
Saurabh <141862550+Code-with-saurabh@users.noreply.github.com>
2023-10-28 23:20:53 +0530

    Update and rename liner seach.cpp to Copy string .c
    String Copy without using bulitin function

--- Copy string .c
+++ Copy string .c
@@ -1,14 +1,13 @@
-#include <iostream>
-
+#include <stdio.h>
 int main () {
- char str[100],Rstr[100];
+ char str[100],Copy_str[100];
  int i=0,count=0;
  printf("Enter String : ");
  scanf("%s",&str);
 for(i;str!=NULL;i++) {
-	str[i]=Rstr[i];
+	str[i]=Copy_str[i];
 }
-printf("The Copyed String is %s ",Rstr);
+printf("The Copyed String is %s ",Copy_str);
 	return 0;
 }
 
